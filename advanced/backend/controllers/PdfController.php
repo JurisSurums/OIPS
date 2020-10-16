21b6cec5c834dd34750c9942ead816a1e2322531
juris <juris.surums@gmail.com>
2020-10-16 15:00:35 +0300

    16.10.2020

--- advanced/backend/controllers/PdfController.php
+++ advanced/backend/controllers/PdfController.php
@@ -15,8 +15,6 @@ class PdfController extends Controller
         $allFiles = FileHelper::findDirectories('uploads/', ['recursive' => false]);
         if(Yii::$app->request->isPost) { // pressed the button
             $y = Yii::$app->request->post();
-/*            var_dump($y);
-            exit;*/
             $this->actionUpload($y["namo"]);
         }
         return $this->render('sucess', ['allFiles' => $allFiles]);
@@ -27,21 +25,15 @@ class PdfController extends Controller
         $model = new UploadForm();
         $allFiles = FileHelper::findDirectories('uploads/', ['recursive' => false]);
         $dirname = $model->dir($namo, $allFiles); //returns selected - svetku uvertire
-
         $allFiles = FileHelper::findDirectories($dirname, ['recursive' => false]);
-        Yii::$app->request->isPost = false;
-        if (Yii::$app->request->isPost) { // pressed the button
-            var_dump("hehe");
-            exit;
-            $post = Yii::$app->request->post();
-            $model->pdfFile = UploadedFile::getInstance($model, 'pdfFile');
-            if ($model->upload($namo, $allFiles)) {
-                // file is uploaded successfully
-                return $this->render('sucess');
-            }
-        }
-        var_dump("heheeeee");
-        exit;
-        return $this->render('upload', ['model' => $model, 'allFiles' => $allFiles]);
+/*        var_dump($allFiles);
+        exit;*/
+        return $this->render('/pdf/upload', ['model' => $model, 'allFiles' => $allFiles]);
+/*        $post = Yii::$app->request->post();
+        $model->pdfFile = UploadedFile::getInstance($model, 'pdfFile');
+        if ($model->upload($namo, $allFiles)) {
+            // file is uploaded successfully
+            return $this->render('sucess');
+        }*/
     }
 }
